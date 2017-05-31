6bf9099a8033fd4e584bca5bc129f5399936281d
cperezavi <cperezavi@gmail.com>
2017-05-31 15:32:37 -0500

    Update stats.sql

--- stats.sql
+++ stats.sql
@@ -3,4 +3,4 @@ EXEC DBMS_STATS.gather_dictionary_stats;
 EXEC DBMS_STATS.gather_fixed_objects_stats;
 EXEC DBMS_STATS.gather_system_stats;
 EXEC DBMS_STATS.gather_database_stats(estimate_percent => 15, cascade => TRUE);
-EXEC DBMS_STATS.gather_schema_stats('INBROKER_FASE2', estimate_percent => 15, cascade => TRUE);
+EXEC DBMS_STATS.gather_schema_stats('', estimate_percent => 15, cascade => TRUE);
